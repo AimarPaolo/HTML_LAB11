f33ae5717271b26893905fbbbd4e87db444a6082
Paolo Aimar <s297424@studenti.polito.it>
2024-06-03 22:00:35 +0200

    Esercizi 1/2) utilizzando il primo metodo è possibili utilizzare la forma puntata per inserire un valore nella query. Sarebbe più sicuro utilizzare una query parametrica...

---
+++ LAB11_2.php
@@ -0,0 +1,59 @@
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pagina per acquistare i biglietti</title>
+        <meta name="author" content="Paolo Aimar">
+        <meta name="keywords" lang="it" content="html">
+        <meta name="description" content="Pagina di acquisto dei biglietti">
+        <meta http-equiv="refresh" content="600">
+    </head>
+    <body>
+        <h1>Auto nuove</h1>
+        <?php
+            //per prima cosa mi prendo la connessione, inserendo il valore del localhost 172.17.0.5
+            $con = mysqli_connect("172.17.0.5", "uReadWrite", "SuperPippo!!!", "auto");
+          
+         if (mysqli_connect_errno()) 
+            echo "<p>Errore connessione al DBMS: ".mysqli_connect_error()."</p>\n";
+         else
+         {
+        ?>
+            <table>
+                <caption>Parco Auto</caption>
+                <tr>
+                    <th>Nome</th>
+                    <th>Quantit&agrave;</th>
+                    <th>Costo</th>
+                </tr>
+                <?php
+                    if(isset($_REQUEST['costo']) && $_REQUEST['costo'] != ""){
+                        $costo = $_REQUEST['costo'];
+                        //in questo caso utilizzo questo metodo, si potrebbero utilizzare anche le query parametriche che sono molto più sicure 
+                    //ed evitato SQL injection
+                        $query = "SELECT * FROM auto_nuove WHERE COSTO <'". $costo ."'";
+                    }else{
+                        //scrivo la query ed estraggo dal database i valori 
+                    $query = "SELECT * FROM auto_nuove";
+                    }
+                    $result = mysqli_query($con, $query);
+                    if(!$result)
+                            echo "<tr>\n<td>Errore query fallita: ".mysqli_error($con)."</td>\n</tr>\n";
+                        else
+                        {
+                            while($row = mysqli_fetch_assoc($result))
+                                echo "<tr>\n<td>$row[NOME]</td>\n<td>$row[QUANTITA]</td>\n<td>".number_format($row['COSTO'],2,',','.')." &euro;</td>\n</tr>\n";
+                             
+                            mysqli_free_result($result);
+                        }                    
+            
+                        if(!mysqli_close($con)){
+                            echo "<p>La connessione non si riesce a chiudere, errore.</p>";
+                        }
+                ?>
+                
+            </table> 
+        <?php
+            }
+        ?>       
+    </body>
+</html>
